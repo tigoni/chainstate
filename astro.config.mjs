48f37d1ac304ec6eadfcbf82779e3cfb6fdbdbdd
KM <kmuhindi@protonmail.ch>
2025-06-01 00:22:41 +0300

    Remove GitHub Pages adapter configuration and adjust base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	base: '/chainstate/',
-	adapter: githubPages({
-		site: 'https://tigoni.github.io/chainstate',
-	}),
+	base: '/chainstate',
 	site: 'https://tigoni.github.io/chainstate',
 	integrations: [mdx(), sitemap()],
 });
