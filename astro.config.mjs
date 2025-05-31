8e650d53519bf80de6da9517dca1aad345f45cbc
KM <kmuhindi@protonmail.ch>
2025-06-01 00:29:18 +0300

    Fix base path formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	base: '/chainstate',
+	base: 'chainstate/',
 	site: 'https://tigoni.github.io/chainstate',
 	integrations: [mdx(), sitemap()],
 });
